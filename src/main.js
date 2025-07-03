b0403f02e2787a08dd097e4f5fbf065f925e8c25
farahShaf <farah@liveschoolinc.com>
2025-07-03 17:41:29 -0600

    updated api key for google maps

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import * as VueGoogleMaps from "vue2-google-maps";
+// import ReactiveStorage from "vue-reactive-localstorage";
 import Vuex from 'vuex';
 import store from './store'
 
@@ -27,6 +29,28 @@ Vue.use(VueGoogleMaps, {
         libraries: "places" // necessary for places input
     }
 });
+// Vue.use(ReactiveStorage, {
+//     properties: [
+//         {
+//             formatted_address: '412 West 9000 South, Sandy, UT 84070\n' +
+//             'Sandy Sandy Utah United States',
+//             full_name: 'Timcoat Tim',
+//             price: '350k',
+//             description: 'Single Home, Built in 1998',
+//             lat: '40.5886089',
+//             lng: '-111.9034992'
+//         },
+//         {
+//             formatted_address: 'South Salt Lake, UT 84115\n' +
+//             'South Salt Lake South Salt Lake, Salt Lake City Utah United States',
+//             full_name: 'Chris Hornok',
+//             price: '470k',
+//             description: 'twin home, Built in 2010',
+//             lat: '40.6973223',
+//             lng: '-111.8784961'
+//         }
+//     ]
+// })
 
 Vue.config.productionTip = false
 
@@ -57,7 +81,6 @@ const router = new VueRouter({
     routes
 })
 new Vue({
-  mode: 'history',
   store,
   render: h => h(App),
     router
